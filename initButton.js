08b2b58581257e194d8bf9caf245e9ee6718b945
inescayolla <ines.cayolla@gmail.com>
2022-09-20 17:26:01 +0100

    Html-struture-basics

---
+++ initButton.js
@@ -0,0 +1,13 @@
+const button = document.querySelector('.button');
+const text = document.querySelector('.text');
+
+const first = "<h1>Hi, my name is Ines! <br> I'm a fullstack developer and...<br></h1><p>(1/2)</p>"
+const second = "<h1>now I know JavaScript and have to complete the text. Ouch!<br></h1><p>(2/2)</p>"
+
+button.addEventListener('click', () => {
+  if (text.innerHTML === second) {
+    text.innerHTML = first;
+  } else {
+    text.innerHTML = second;
+  }
+});
